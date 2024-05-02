5a8df867f639862227df4cdc0e890aea7f20a37e
HansJao <vigorhan@gmail.com>
2024-05-02 16:24:14 +0800

    Update

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -24,4 +24,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "Products",
+    pattern: "{controller=Products}/{action=Intro}/{id?}");
+
 app.Run();
